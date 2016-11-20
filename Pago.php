878bdb3830010a0e2d65d64bab0903e74a67e018
RodrigoDaboin <daboin.rodrigo@gmail.com>
2016-11-20 03:31:13 -0300

    Add files via upload

---
+++ Pago.php
@@ -0,0 +1,7 @@
+<?php
+//function fnValidateCreditCard($cc){
+	#eg. 718486746312031
+	//return preg_match('/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6011[0-9]{12}|3(?:0[0-5]|[68][0-9])[0-9]{11}|3[47][0-9]{13})$/', $cc);
+//	}
+header ("Location:pagorealizado.html");
+?>
